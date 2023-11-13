513cf57c3b35d1550e321317ba3ae27038e9bd88
Serina Yamamoto <serinayamamoto158@gmail.com>
2023-11-13 23:28:40 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const nextConfig = {
-  output: 'standalone',
   experimental: {
     appDir: true,
   },
+  output: process.env.STANDALONE === "true" ? "standalone" : "export",
+  // reactStrictMode: true,
+  trailingSlash: true,
 }
