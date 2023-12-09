940b5aa3c7dabcb1735af8d59573d359a20b8b39
Serina Yamamoto <serinayamamoto158@gmail.com>
2023-12-09 14:03:53 +0900

    テスト修正

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -5,7 +5,7 @@ describe('Home', () => {
   it('renders a heading', () => {
     render(<Home />)
     const heading = screen.getByRole('heading', {
-      name: /Let\'s search for books/i,
+      name: /Let's search for books by title/i,
     })
     expect(heading).toBeInTheDocument()
   })
