f3d56176566642a7d43051f5ae05dbe691c04a5c
Serina Yam <64587946+serina-yam@users.noreply.github.com>
2024-08-14 13:22:05 +0900

    環境変数の設定

--- next.config.js
+++ next.config.js
@@ -4,4 +4,8 @@ const nextConfig = {
   },
   output: 'standalone',
   trailingSlash: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  },
 }
